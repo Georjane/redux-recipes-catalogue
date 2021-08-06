216fc9bbc6c332a3627b49b961a971bfadf8389c
Georjane <pzgeorjane@gmail.com>
2021-08-06 12:44:34 +0100

    Add routes for app

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './components/App';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/details" component={Details} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
