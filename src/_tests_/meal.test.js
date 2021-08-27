d6bd2077e75bb9c7fd03f54a5a65101ab6305c8c
Georjane <pzgeorjane@gmail.com>
2021-08-27 20:03:49 +0100

    fix eslint error

--- src/_tests_/meal.test.js
+++ src/_tests_/meal.test.js
@@ -3,8 +3,9 @@ import renderer from 'react-test-renderer';
 import Meal from '../components/Meal';
 
 it('renders meal page', () => {
+  const meal = {'strMeal': 'Chick-Fil-A Sandwich', 'strMealThumb': 'https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg'}
   const strMeal = 'Chicken soup';
   const strMealThumb = 'https://www.themealdb.com/images/media/meals/sbx7n71587673021.jpg';
-  const tree = renderer.create(<Meal meal={strMeal, strMealThumb} />);
+  const tree = renderer.create(<Meal meal={strMeal} />);
   expect(tree).toMatchSnapshot();
 });
